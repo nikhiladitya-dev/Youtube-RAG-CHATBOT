cd632002170ddbfeafb42dd26368427e20b97acb
nikhiladitya-dev <nikhiladitya.dev@gmail.com>
2026-08-04 17:21:47 +0530

    Update run.py

--- panda-chatbot/run.py
+++ panda-chatbot/run.py
@@ -7,7 +7,7 @@ def main():
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000)),
+        port=int(os.environ.get("PORT", 10000)),
         reload=False,
     )
 
