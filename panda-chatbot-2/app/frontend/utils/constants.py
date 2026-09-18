b175e6455c824f228f300a2da5a69f0c8656c94a
nikhiladitya-dev <nikhiladitya.dev@gmail.com>
2026-09-18 22:13:00 +0530

    Update constants.py

--- panda-chatbot-2/app/frontend/utils/constants.py
+++ panda-chatbot-2/app/frontend/utils/constants.py
@@ -1,7 +1,10 @@
-API_URL = "http://127.0.0.1:8000/api/v1"
+import os
+
+API_URL = os.getenv(
+    "API_URL",
+    "http://127.0.0.1:8000/api/v1"
+)
 
 APP_NAME = "PANDA CHATBOT"
-
 TAGLINE = "Skip the Watch. Ask the Video."
-
 APP_ICON = "🐼"
